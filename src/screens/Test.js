683b1cef5d57d56d8b7114238638622bba475491
Lucas Di Mattia <lucasdimattia@gmail.com>
2020-09-14 23:34:26 -0300

    Text styles adjustment

--- src/screens/Test.js
+++ src/screens/Test.js
@@ -36,7 +36,7 @@ class Test extends React.Component {
             <p className="test-text-11" style={{ 'marginBottom' : '40px' }} >Text Preset 11</p>
           </div>
 
-          <div style={{ 'display':'none' }}>
+          <div>
             <p className="test-text-1" style={{ 'marginBottom' : '40px' }} >Text Preset 1 · Alta Córdoba</p>
             <p className="test-text-2" style={{ 'marginBottom' : '40px' }} >Text Preset 2 · I pledge my life and honor to the Night's Watch, for this night and all the nights to come. I pledge my life and honor to the Night's Watch, for this night and all the nights to come. When you play the game of thrones, you win or you die. What is dead may never die. Bastards are born of passion, aren't they? I pledge my life and honor to the Night's Watch, for this night and all the nights to come. When you play the game of thrones, you win or you die. What is dead may never die. Bastards are born of passion, aren't they?</p>
             <p className="test-text-3" style={{ 'marginBottom' : '40px' }} >Text Preset 3 · All men must die. House Tarly of Horn Hill Never Resting. I pledge my life and honor to the Night's Watch, for this night and all the nights to come. When you play the game of thrones, you win or you die. What is dead may never die. Bastards are born of passion, aren't they? I pledge my life and honor to the Night's Watch, for this night and all the nights to come. When you play the game of thrones, you win or you die. What is dead may never die. Bastards are born of passion, aren't they?</p>
@@ -51,61 +51,61 @@ class Test extends React.Component {
             <p className="test-text-12" style={{ 'marginBottom' : '40px' }} >Text Preset 12 · A good act does not wash out the bad, nor a bad act the good. I pledge my life and honor to the Night's Watch, for this night and all the nights to come. When you play the game of thrones, you win or you die. What is dead may never die. Bastards are born of passion, aren't they? I pledge my life and honor to the Night's Watch, for this night and all the nights to come. When you play the game of thrones, you win or you die. What is dead may never die. Bastards are born of passion, aren't they?</p>
           </div>
 
-          <div style={{ 'display':'none' }}>
-            <Icon name="visual-arts" color="#44DD00" width="24" height="24" />
-            <Icon name="cinema" color="#44DD00" width="24" height="24" />
-            <Icon name="theatre" color="#44DD00" width="24" height="24" />
-            <Icon name="science" color="#44DD00" width="24" height="24" />
-            <Icon name="kids" color="#44DD00" width="24" height="24" />
-            <Icon name="architecture" color="#44DD00" width="24" height="24" />
-            <Icon name="park" color="#44DD00" width="24" height="24" />
-            <Icon name="museum" color="#44DD00" width="24" height="24" />
-            <Icon name="design" color="#44DD00" width="24" height="24" />
-            <Icon name="design-alt-1" color="#44DD00" width="24" height="24" />
-            <Icon name="shopping" color="#44DD00" width="24" height="24" />
-            <Icon name="bike" color="#44DD00" width="24" height="24" />
-            <Icon name="church" color="#44DD00" width="24" height="24" />
-            <Icon name="sustainable" color="#44DD00" width="24" height="24" />
-            <Icon name="sustainable-alt-1" color="#44DD00" width="24" height="24" />
-            <Icon name="sustainable-alt-2" color="#44DD00" width="24" height="24" />
-            <Icon name="eat-and-drink" color="#44DD00" width="24" height="24" />
-            <Icon name="coffee" color="#44DD00" width="24" height="24" />
-            <Icon name="fair" color="#44DD00" width="24" height="24" />
-            <Icon name="secrets" color="#44DD00" width="24" height="24" />
-            <Icon name="music" color="#44DD00" width="24" height="24" />
-            <Icon name="books" color="#44DD00" width="24" height="24" />
-            <Icon name="food-truck" color="#44DD00" width="24" height="24" />
-            <Icon name="food-truck-alt-1" color="#44DD00" width="24" height="24" />
-            <Icon name="binoculars" color="#44DD00" width="24" height="24" />
-            <Icon name="lamp" color="#44DD00" width="24" height="24" />
-            <Icon name="rock" color="#44DD00" width="24" height="24" />
-            <Icon name="camera" color="#44DD00" width="24" height="24" />
-            <Icon name="carnival" color="#44DD00" width="24" height="24" />
-            <Icon name="carnival-alt-1" color="#44DD00" width="24" height="24" />
-            <Icon name="track" color="#44DD00" width="24" height="24" />
-            <Icon name="diamond" color="#44DD00" width="24" height="24" />
-            <Icon name="globe" color="#44DD00" width="24" height="24" />
-            <Icon name="directions" color="#44DD00" width="24" height="24" />
-            <Icon name="finger" color="#44DD00" width="24" height="24" />
-            <Icon name="calendar" color="#44DD00" width="24" height="24" />
-            <Icon name="information" color="#44DD00" width="24" height="24" />
-            <Icon name="ticket" color="#44DD00" width="24" height="24" />
-            <Icon name="landmark" color="#44DD00" width="24" height="24" />
-            <Icon name="plus" color="#44DD00" width="24" height="24" />
-            <Icon name="cross" color="#44DD00" width="24" height="24" />
-            <Icon name="arrow-left" color="#44DD00" width="24" height="24" />
-            <Icon name="arrow-right" color="#44DD00" width="24" height="24" />
-            <Icon name="carat-left" color="#44DD00" width="24" height="24" />
-            <Icon name="carat-right" color="#44DD00" width="24" height="24" />
-            <Icon name="menu" color="#44DD00" width="24" height="24" />
-            <Icon name="layout-cards" color="#44DD00" width="24" height="24" />
-            <Icon name="layout-grid" color="#44DD00" width="24" height="24" />
-            <Icon name="layout-tiles" color="#44DD00" width="24" height="24" />
-            <Icon name="map" color="#44DD00" width="24" height="24" />
-            <Icon name="map-alt-1" color="#44DD00" width="24" height="24" />
-            <Icon name="home" color="#44DD00" width="24" height="24" />
-            <Icon name="map-pin" color="#44DD00" width="24" height="24" />
-            <Icon name="time" color="#44DD00" width="24" height="24" />
+          <div>
+            <Icon name="visual-arts" color="#0000FF" width="24" height="24" />
+            <Icon name="cinema" color="#0000FF" width="24" height="24" />
+            <Icon name="theatre" color="#0000FF" width="24" height="24" />
+            <Icon name="science" color="#0000FF" width="24" height="24" />
+            <Icon name="kids" color="#0000FF" width="24" height="24" />
+            <Icon name="architecture" color="#0000FF" width="24" height="24" />
+            <Icon name="park" color="#0000FF" width="24" height="24" />
+            <Icon name="museum" color="#0000FF" width="24" height="24" />
+            <Icon name="design" color="#0000FF" width="24" height="24" />
+            <Icon name="design-alt-1" color="#0000FF" width="24" height="24" />
+            <Icon name="shopping" color="#0000FF" width="24" height="24" />
+            <Icon name="bike" color="#0000FF" width="24" height="24" />
+            <Icon name="church" color="#0000FF" width="24" height="24" />
+            <Icon name="sustainable" color="#0000FF" width="24" height="24" />
+            <Icon name="sustainable-alt-1" color="#0000FF" width="24" height="24" />
+            <Icon name="sustainable-alt-2" color="#0000FF" width="24" height="24" />
+            <Icon name="eat-and-drink" color="#0000FF" width="24" height="24" />
+            <Icon name="coffee" color="#0000FF" width="24" height="24" />
+            <Icon name="fair" color="#0000FF" width="24" height="24" />
+            <Icon name="secrets" color="#0000FF" width="24" height="24" />
+            <Icon name="music" color="#0000FF" width="24" height="24" />
+            <Icon name="books" color="#0000FF" width="24" height="24" />
+            <Icon name="food-truck" color="#0000FF" width="24" height="24" />
+            <Icon name="food-truck-alt-1" color="#0000FF" width="24" height="24" />
+            <Icon name="binoculars" color="#0000FF" width="24" height="24" />
+            <Icon name="lamp" color="#0000FF" width="24" height="24" />
+            <Icon name="rock" color="#0000FF" width="24" height="24" />
+            <Icon name="camera" color="#0000FF" width="24" height="24" />
+            <Icon name="carnival" color="#0000FF" width="24" height="24" />
+            <Icon name="carnival-alt-1" color="#0000FF" width="24" height="24" />
+            <Icon name="track" color="#0000FF" width="24" height="24" />
+            <Icon name="diamond" color="#0000FF" width="24" height="24" />
+            <Icon name="globe" color="#0000FF" width="24" height="24" />
+            <Icon name="directions" color="#0000FF" width="24" height="24" />
+            <Icon name="finger" color="#0000FF" width="24" height="24" />
+            <Icon name="calendar" color="#0000FF" width="24" height="24" />
+            <Icon name="information" color="#0000FF" width="24" height="24" />
+            <Icon name="ticket" color="#0000FF" width="24" height="24" />
+            <Icon name="landmark" color="#0000FF" width="24" height="24" />
+            <Icon name="plus" color="#0000FF" width="24" height="24" />
+            <Icon name="cross" color="#0000FF" width="24" height="24" />
+            <Icon name="arrow-left" color="#0000FF" width="24" height="24" />
+            <Icon name="arrow-right" color="#0000FF" width="24" height="24" />
+            <Icon name="carat-left" color="#0000FF" width="24" height="24" />
+            <Icon name="carat-right" color="#0000FF" width="24" height="24" />
+            <Icon name="menu" color="#0000FF" width="24" height="24" />
+            <Icon name="layout-cards" color="#0000FF" width="24" height="24" />
+            <Icon name="layout-grid" color="#0000FF" width="24" height="24" />
+            <Icon name="layout-tiles" color="#0000FF" width="24" height="24" />
+            <Icon name="map" color="#0000FF" width="24" height="24" />
+            <Icon name="map-alt-1" color="#0000FF" width="24" height="24" />
+            <Icon name="home" color="#0000FF" width="24" height="24" />
+            <Icon name="map-pin" color="#0000FF" width="24" height="24" />
+            <Icon name="time" color="#0000FF" width="24" height="24" />
           </div>
         </div>
       </div>
