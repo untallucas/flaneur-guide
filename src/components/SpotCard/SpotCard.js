b14cbd546e453cb3c1f4c9921c659dd88e490945
Lucas Di Mattia <lucasdimattia@gmail.com>
2020-09-16 03:47:05 -0300

    Lazy load images

--- src/components/SpotCard/SpotCard.js
+++ src/components/SpotCard/SpotCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 import HighlightBadge from '../HighlightBadge/HighlightBadge'
 
@@ -23,15 +24,26 @@ const SpotCard = function ({ spot }) {
   let url = '/lugares/' + spot.slug
   let printname = spot.shorttitle ? spot.shorttitle : spot.title
   let image = spot.poster_url ? spot.poster_url[0].url : ''
-  let highlight 
+  let posterColor = spot.poster_color ? spot.poster_color : '#CCCCCC'
+  let highlight
   if(spot.highlight){
     highlight = <HighlightBadge/>
-  } 
+  }
 
   return (
     <div className="SpotCard">
       <Link to={ url } className="SpotCard__Link">
-        <img className="SpotCard__Image" src={ image } alt={ spot.printname } />
+        <LazyLoadImage
+          src={ image }
+          alt={ spot.printname }
+          height={ '100%' }
+          width={ '100%' }
+          effect='opacity'
+          threshold= { 0 }
+          className='SpotCard__Image'
+          wrapperClassName='SpotCard__ImageWrapper'
+          wrapperProps={{ style: { backgroundColor: posterColor } }}
+        />
         <h3 className="SpotCard__Title">{ printname }</h3>
         <p className="SpotCard__Subtitle">{ spot.hood }</p>
         { highlight }
