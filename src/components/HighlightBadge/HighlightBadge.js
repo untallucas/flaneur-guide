6e7c9ce1918d9a926a4c7bdeddeba154aeaff28d
Lucas Di Mattia <lucasdimattia@gmail.com>
2020-09-23 23:14:43 -0300

    Change Highlight badge again

--- src/components/HighlightBadge/HighlightBadge.js
+++ src/components/HighlightBadge/HighlightBadge.js
@@ -1,13 +1,11 @@
 import React from 'react'
 
-import Icon from '../Icon/Icon'
-
 import './HighlightBadge.scss'
 
 const HighlightBadge = props => {
   return (
     <div className="HighlightBadge">
-      <Icon className="HighlightBadge__Icon" name="finger" color="#FF00CC" width="40" height="40" />
+      RECOMENDADO
     </div>
   )
 }
