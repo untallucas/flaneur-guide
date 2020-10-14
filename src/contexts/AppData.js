f946af1a2cddb49b9b7a40782708a809e6d12dbf
Lucas Di Mattia <lucasdimattia@gmail.com>
2020-10-14 02:59:39 -0300

    Social share + metadata + hide context

--- src/contexts/AppData.js
+++ src/contexts/AppData.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const AppData = React.createContext({ })
+const CityDataEndpoint = process.env.REACT_APP_CITY_ENDPOINT
+const TaxonomiesDataEndpoint = process.env.REACT_APP_TAXONOMIES_ENDPOINT
+const SpotsDataEndpoint = process.env.REACT_APP_SPOTS_ENDPOINT
 
 class AppDataProvider extends React.Component {
   state = {
@@ -11,7 +14,7 @@ class AppDataProvider extends React.Component {
 
   fetchCityData() {
     // const proxyurl = 'https://cors-anywhere.herokuapp.com/'
-    const url = 'https://notion-api.splitbee.io/v1/table/f4b196daa2b0472a89da476454a7227c?v=3496157f3ecd4b8cae25881d7963ca56'
+    const url = CityDataEndpoint; // 'https://notion-api.splitbee.io/v1/table/f4b196daa2b0472a89da476454a7227c?v=3496157f3ecd4b8cae25881d7963ca56'
     fetch(url)
       .then(cityResponse => cityResponse.json())
       .then(getCityData =>
@@ -23,7 +26,7 @@ class AppDataProvider extends React.Component {
 
   fetchTaxonomiesData() {
     // const proxyurl = 'https://cors-anywhere.herokuapp.com/'
-    const url = 'https://notion-api.splitbee.io/v1/table/fa802c44459d4c7c902ada4cde30fed3?v=025e59130a404965bdd4854504da64b8'
+    const url = TaxonomiesDataEndpoint; // 'https://notion-api.splitbee.io/v1/table/fa802c44459d4c7c902ada4cde30fed3?v=025e59130a404965bdd4854504da64b8'
     fetch(url)
       .then(taxonomiesResponse => taxonomiesResponse.json())
       .then(getTaxonomiesData =>
@@ -35,7 +38,7 @@ class AppDataProvider extends React.Component {
 
   fetchSpotsData() {
     // const proxyurl = 'https://cors-anywhere.herokuapp.com/'
-    const url = 'https://notion-api.splitbee.io/v1/table/06a53a97c7704fac82906509da256483?v=68cac7916d3f43a08f824ea21fda8fbf'
+    const url = SpotsDataEndpoint; // 'https://notion-api.splitbee.io/v1/table/06a53a97c7704fac82906509da256483?v=68cac7916d3f43a08f824ea21fda8fbf'
     fetch(url)
       .then(spotsResponse => spotsResponse.json())
       .then(getSpotsData =>
