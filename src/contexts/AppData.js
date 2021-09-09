166f41f3adf64bc6d52be5ca1f0d332767c9b180
Lucas Di Mattia <lucasdimattia@gmail.com>
2021-09-08 23:29:45 -0300

    Clean up comments

--- src/contexts/AppData.js
+++ src/contexts/AppData.js
@@ -13,7 +13,6 @@ class AppDataProvider extends React.Component {
   }
 
   fetchCityData() {
-    // const proxyurl = 'https://cors-anywhere.herokuapp.com/'
     const url = CityDataEndpoint; // 'https://notion-api.splitbee.io/v1/table/f4b196daa2b0472a89da476454a7227c?v=3496157f3ecd4b8cae25881d7963ca56'
     fetch(url)
       .then(cityResponse => cityResponse.json())
@@ -25,7 +24,6 @@ class AppDataProvider extends React.Component {
   }
 
   fetchTaxonomiesData() {
-    // const proxyurl = 'https://cors-anywhere.herokuapp.com/'
     const url = TaxonomiesDataEndpoint; // 'https://notion-api.splitbee.io/v1/table/fa802c44459d4c7c902ada4cde30fed3?v=025e59130a404965bdd4854504da64b8'
     fetch(url)
       .then(taxonomiesResponse => taxonomiesResponse.json())
@@ -37,7 +35,6 @@ class AppDataProvider extends React.Component {
   }
 
   fetchSpotsData() {
-    // const proxyurl = 'https://cors-anywhere.herokuapp.com/'
     const url = SpotsDataEndpoint; // 'https://notion-api.splitbee.io/v1/table/06a53a97c7704fac82906509da256483?v=68cac7916d3f43a08f824ea21fda8fbf'
     fetch(url)
       .then(spotsResponse => spotsResponse.json())
